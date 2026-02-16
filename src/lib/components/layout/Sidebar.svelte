b12725dba02d8cc221cf14c9c983602357e58b97
Skaue <marcus@skaue.com>
2026-02-16 12:46:25 +0100

    Added solwr logo to intended places

--- src/lib/components/layout/Sidebar.svelte
+++ src/lib/components/layout/Sidebar.svelte
@@ -670,7 +670,7 @@
 					>
 						<div class=" self-center flex items-center justify-center size-9">
 							<img
-								src="{WEBUI_BASE_URL}/static/favicon.png"
+								src="{WEBUI_BASE_URL}/static/solwr.png"
 								class="sidebar-new-chat-icon size-6 rounded-full group-hover:hidden"
 								alt=""
 							/>
@@ -867,8 +867,7 @@
 					on:click={newChatHandler}
 				>
 					<img
-						crossorigin="anonymous"
-						src="{WEBUI_BASE_URL}/static/favicon.png"
+						src="{WEBUI_BASE_URL}/static/solwr.png"
 						class="sidebar-new-chat-icon size-6 rounded-full"
 						alt=""
 					/>
